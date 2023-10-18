bc0e6be6f5c75c296f1796aa4e0a92f2ea884c5e
Riyaz Haque <haque1@llnl.gov>
2023-10-18 14:31:43 -0700

    Delete ol raja-perf config files

--- raja-perf/cts1/configs/execute_experiment.tpl
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH -N {n_nodes}
-#SBATCH -n {n_ranks}
-
-cd {experiment_run_dir}
-
-{spack_setup}
-
-{command}
--- raja-perf/rambler/ats2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-TEST_NAME=raja-perf \
-TEST_CONFIG_NAME=ats2 \
-TEST_SOURCE_DIR= \
-TEST_WORKING_ROOT= \
-./rambler.sh
-#TEST_SOURCE_DIR=/${HOME}/benchpark/${TEST_NAME} \
-#TEST_WORKING_ROOT=/p/lustre/${USER} \
