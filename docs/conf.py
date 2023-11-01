40cae13289887fd257816f2fc911b30a3ad7c4a3
Stephanie Brink <brink2@llnl.gov>
2023-11-01 13:04:31 -0700

    lint and formatting with flake8 and black
    - add CI yaml config for formatting
    - add pyproject.toml file for black config, .flake8 for flake config
    - format benchpark and conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,23 +6,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Benchpark'
-copyright = '2023, LLNS LLC'
-author = 'Olga Pearce, Alec Scott, Peter Scheibel, Greg Becker, Riyaz Haque, and Nathan Hanford'
+project = "Benchpark"
+copyright = "2023, LLNS LLC"
+author = "Olga Pearce, Alec Scott, Peter Scheibel, Greg Becker, Riyaz Haque, and Nathan Hanford"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx_rtd_theme',
+    "sphinx_rtd_theme",
 ]
 
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', '.spack-env']
-
-
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".spack-env"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
