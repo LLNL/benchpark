1832420fb48ac9c76e859c1ba6410ed7a058b8f4
Olga T. Pearce <pearce8@llnl.gov>
2023-12-01 07:53:14 -0600

    starting a llnl-weekly suite

---
+++ suites/llnl-weekly/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+../../bin/benchpark setup saxpy/openmp x86 ../../../llnl-weekly
+../../bin/benchpark setup amg2023/openmp x86 ../../../llnl-weekly
+../../bin/benchpark setup raja-perf/openmp x86 ../../../llnl-weekly
