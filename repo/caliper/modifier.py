a148e2c74e7c056d8a1ed0ea5e3a8e4c678e1a44
pearce8 <pearce8@llnl.gov>
2023-12-11 13:24:35 -0600

    Removing postprocessing

--- repo/caliper/modifier.py
+++ repo/caliper/modifier.py
@@ -18,12 +18,14 @@ class Caliper(SpackModifier):
 
     maintainers('olgapearce')
 
-    mode('spot', description='Mode for collecting time')
+    mode('spot', description='Mode for collecting time only')
 
     env_var_modification('CALI_CONFIG', 'spot(output={experiment_run_dir}/{experiment_name}.cali)', method='set', modes=['spot'])
 
     _log_file = '{experiment_run_dir}/.caliper_fom'
     _cali_datadir = '{experiment_run_dir}/{experiment_name}.cali'
+
+    # FIXME: Should we just remove FOM from here entirely?
     _target_fom_name = '{target_fom_name}'
 
     # This will feed into an external profiler/data aggregator
@@ -34,35 +36,6 @@ class Caliper(SpackModifier):
 
     required_package('caliper')
 
+    # FIXME: Can we just remove FOM from here, since that will only happen in post-processing?
     figure_of_merit('{target_fom_name}', fom_regex='{target_fom_name} = (?P<fom>.*)', log_file=_log_file,
                     units='', group_name='fom')
-
-    # FIXME: This should be provided by the system
-    # Handle this in Spack?
-    def _load_thicket(self):
-        # the plot thickets!!!
-        import matplotlib.pyplot as plt
-
-        import sys
-        import platform
-
-        input_deploy_dir_str = "/usr/gapps/spot/dev/"
-        machine = platform.uname().machine
-
-        sys.path.append(input_deploy_dir_str + "/hatchet-venv/" + machine + "/lib/python3.9/site-packages")
-        sys.path.append(input_deploy_dir_str + "/hatchet/" + machine)
-        sys.path.append(input_deploy_dir_str + "/thicket-playground-dev")
-
-    # TODO: add hooks to customize this function per experiment
-    # add additional python scripts in configs? 
-    def _prepare_analysis(self, workspace):
-        self._load_thicket()
-
-        import hatchet as ht
-        import thicket as tt
-
-        with open(self.expander.expand_var(self._log_file), 'w+') as f:
-            target_fom_name = self.expander.expand_var(self._target_fom_name)
-            t_ens = tt.Thicket.from_caliperreader(self.expander.expand_var(self._cali_datadir))
-            fom = t_ens.metadata[target_fom_name].iloc[0]
-            f.write("{} = {:.6E}".format(target_fom_name, fom))
