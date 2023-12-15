ad6b19b01068b8a87f2b17f89f720be770ed2030
Olga T. Pearce <pearce8@llnl.gov>
2023-12-15 13:58:03 -0600

    Adding Kripke to Benchpark

---
+++ repo/kripke/application.py
@@ -0,0 +1,51 @@
+from ramble.appkit import *
+
+import sys
+
+class Kripke(SpackApplication):
+    """Kripke benchmark uses RAJA Portability Layer"""
+    name = "Kripke"
+
+    tags = ["kripke, RAJA"]
+
+    executable('p1', 'kripke' +
+                     ' -P {px} {py} {pz}' +
+                     ' -n {nx} {ny} {nz}' +
+                     ' -problem 1'        +
+                     ' -keepT', use_mpi=True)
+
+    executable('p2', 'kripke' +
+                     ' -P {px} {py} {pz}' +
+                     ' -n {nx} {ny} {nz}' +
+                     ' -problem 2'        +
+                     ' -keepT', use_mpi=True)
+
+    workload('problem1', executables=['p1'])
+    workload('problem2', executables=['p2'])
+
+    workload_variable('px', default='2',
+                      description='px',
+                      workloads=['problem1', 'problem2'])
+    workload_variable('py', default='2',
+                      description='py',
+                      workloads=['problem1', 'problem2'])
+    workload_variable('pz', default='2',
+                      description='pz',
+                      workloads=['problem1', 'problem2'])
+    workload_variable('nx', default='220',
+                      description='nx',
+                      workloads=['problem1', 'problem2'])
+    workload_variable('ny', default='220',
+                      description='ny',
+                      workloads=['problem1', 'problem2'])
+    workload_variable('nz', default='220',
+                      description='nz',
+                      workloads=['problem1', 'problem2'])
+
+    figure_of_merit('Figure of Merit (FOM)', log_file='{experiment_run_dir}/{experiment_name}.out', fom_regex=r'Figure of Merit \(FOM\):\s+(?P<fom>[0-9]+\.[0-9]*(e^[0-9]*)?)', group_name='fom', units='')
+
+    #TODO: Fix the FOM success_criteria(...)
+    success_criteria('pass', mode='string', match=r'Figure of Merit \(FOM\)', file='{experiment_run_dir}/{experiment_name}.out')
+
+    def evaluate_success(self):
+      return True
