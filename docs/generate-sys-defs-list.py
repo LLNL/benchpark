86a585777b7b7fa25266d4f8b6c6df46fc944691
Stephanie Brink <brink2@llnl.gov>
2024-01-03 12:51:14 -0800

    parse sys definitions yaml to table in docs
    - add script to parse system_definitions.yaml into csv file
    
    docs:
    - build also updates csv of current system definitions
    - expand width of webpage in custom.css to reduce left/right scroll
    - add new section header for available specs with page for current list of
      system definitions
    
    schema:
    - leave non-applicable fields blank instead of "None" or "" -- this will show
      in the table as empty
    - system-tested.description value should be "text <URL>"

---
+++ docs/generate-sys-defs-list.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+
+import yaml
+import glob
+import pandas as pd
+
+
+def main():
+    sysconfig_yaml_files = glob.glob(
+        "../configs/**/system_definition.yaml", recursive=True
+    )
+
+    df_list = []
+    for f in sysconfig_yaml_files:
+        with open(f, "r") as stream:
+            try:
+                data = yaml.safe_load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+
+            tmp_df = pd.json_normalize(data, max_level=2)
+            df_list.append(tmp_df)
+
+    df = pd.concat(df_list)
+
+    # Data formatting: converts system-tested.description yaml value to rst
+    # format for external links
+    df.loc[
+        df["system_definition.system-tested.description"].notna(),
+        "system_definition.system-tested.description",
+    ] = (
+        "`"
+        + df.loc[
+            df["system_definition.system-tested.description"].notna(),
+            "system_definition.system-tested.description",
+        ].astype(str)
+        + "`_"
+    )
+
+    # Data formatting: converts top500-system-instances yaml list to rst string
+    # of strings (ideally to put 1 per line)
+    list_of_strings = []
+    for i in df["system_definition.top500-system-instances"]:
+        list_of_strings.append(", ".join(item for item in i if item))
+    df.loc[:, "system_definition.top500-system-instances"] = list_of_strings
+
+    # Set index to be system names
+    df.set_index("system_definition.name", inplace=True)
+    df.index.name = ""  # remove system_definition.name from cell
+    df_tpose = df.T
+
+    # Remove system_definition from all field names
+    # (e.g., system_definition.system-tested.description)
+    df_tpose["fielddesc"] = df_tpose.index
+    df_tpose[["first", ""]] = df_tpose["fielddesc"].str.split(".", n=1, expand=True)
+
+    # Add ** to either side of field names for bold rendering in RST
+    df_tpose[""] = "**" + df_tpose[""] + "**"
+
+    # Drop temporary columns
+    df_tpose.drop(["first", "fielddesc"], axis=1, inplace=True)
+
+    # Replace NaN with empty string
+    df_tpose.fillna("", inplace=True)
+
+    # Set index to be field names
+    df_tpose.set_index([""], inplace=True)
+
+    # Write out current system definitions to CSV format
+    df_tpose.to_csv("tables/current-system-definitions.csv")
+
+
+if __name__ == "__main__":
+    main()
