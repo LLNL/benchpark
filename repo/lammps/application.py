9bc80cd9dc55e4fe317dcf8045c93f47ef57d9b7
Olga T. Pearce <pearce8@llnl.gov>
2024-01-09 10:46:10 -0600

    Inhereting from LAMMPS package in Ramble, adding tags

---
+++ repo/lammps/application.py
@@ -0,0 +1,17 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import sys
+
+from ramble.appkit import *
+
+
+class LammpsInherited(SpackApplication):
+
+    tags = ['asc','chemistry','material-science','molecular-dynamics',
+            'fft','particles',
+            'large-scale','multi-node','single-node','sub-node',
+            'mpi','network-collectives','network-point-to-point',
+            'c++','kokkos','fortran','openmp']
