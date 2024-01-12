4915bdd3c67c21d6c2cd69479434c8e9ebc19547
Riyaz Haque <your-email@example.com>
2024-01-12 11:48:17 -0800

    Implementation of caliper top-down modifier

--- modifiers/caliper/modifier.py
+++ modifiers/caliper/modifier.py
@@ -15,19 +15,11 @@ class Caliper(SpackModifier):
 
     maintainers('olgapearce')
 
-    mode('spot', description='Mode for collecting time only')
-
-    mode('spot-topdown', description='Mode for collecting time only')
-
-    mode('spot-cuda', description='Mode for collecting time only')
+    mode('time', description='Mode for collecting time only')
 
     _cali_datafile = '{experiment_run_dir}/{experiment_name}.cali'
 
-    env_var_modification('CALI_CONFIG', 'spot(output={})'.format(_cali_datafile), method='set', modes=['spot'])
-
-    env_var_modification('CALI_CONFIG', 'spot(output={}, topdown.all)'.format(_cali_datafile), method='set', modes=['spot-topdown'])
-
-    env_var_modification('CALI_CONFIG', 'spot(output={}, profile.cuda)'.format(_cali_datafile), method='set', modes=['spot-cuda'])
+    env_var_modification('CALI_CONFIG', 'spot(output={})'.format(_cali_datafile), method='set', modes=['time'])
 
     archive_pattern(_cali_datafile)
 
