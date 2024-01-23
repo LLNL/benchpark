4b5c28795c6c150461382564c37a1089c5c511db
Peter Josef Scheibel <scheibel1@llnl.gov>
2024-01-23 13:25:22 -0800

    needed style fixes

--- lib/scripts/merge.py
+++ lib/scripts/merge.py
@@ -4,8 +4,8 @@ import sys
 
 def main():
     """This ramble-python script can be used to merge two
-       ramble YAML configs with overlapping sections into
-       a single config file.
+    ramble YAML configs with overlapping sections into
+    a single config file.
     """
     f1, f2, output = sys.argv[1], sys.argv[2], sys.argv[3]
 
@@ -13,7 +13,7 @@ def main():
     c2 = cfg.read_config_file(f2)
 
     cfg.merge_yaml(c1, c2)
-    with open(output, 'w') as outstream:
+    with open(output, "w") as outstream:
         syaml.dump_config(c1, outstream)
 
 if __name__ == "__main__":
