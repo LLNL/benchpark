faae471035ee0c9b68fe20862e2ef662fa7c8f24
Alec Scott <scott112@llnl.gov>
2024-04-01 09:26:22 -0700

    Add custom css rules to better color the docs like the logo

--- docs/conf.py
+++ docs/conf.py
@@ -43,3 +43,6 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_css_files = ["css/custom.css"]
 html_logo = "_static/images/benchpark-dark.svg"
+html_theme_options = {
+    'logo_only': True
+}
