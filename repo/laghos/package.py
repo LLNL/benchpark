0ab6bd1b5eeef499d623390ae7e251768f15e217
Riyaz Haque <your-email@example.com>
2024-05-02 23:14:18 -0700

    Use original spack recipe

--- repo/laghos/package.py
+++ repo/laghos/package.py
@@ -15,22 +15,26 @@ class Laghos(MakefilePackage):
 
     tags = ["proxy-app", "ecp-proxy-app"]
 
-    homepage = "https://github.com/wdhawkins/laghos"
-    git = "https://github.com/wdhawkins/Laghos.git"
+    homepage = "https://computing.llnl.gov/projects/co-design/laghos"
+    url = "https://github.com/CEED/Laghos/archive/v1.0.tar.gz"
+    git = "https://github.com/CEED/Laghos.git"
 
-    maintainers("wdhawkins")
+    maintainers("v-dobrev", "tzanio", "vladotomov")
 
     license("BSD-2-Clause")
 
-    version("develop", branch="caliper")
+    version("develop", branch="master")
+    version("3.1", sha256="49b65edcbf9732c7f6c228958620e18980c43ad8381315a8ba9957ecb7534cd5")
+    version("3.0", sha256="4db56286e15b42ecdc8d540c4888a7dec698b019df9c7ccb8319b7ea1f92d8b4")
+    version("2.0", sha256="dd3632d5558889beec2cd3c49eb60f633f99e6d886ac868731610dd006c44c14")
+    version("1.1", sha256="53b9bfe2af263c63eb4544ca1731dd26f40b73a0d2775a9883db51821bf23b7f")
+    version("1.0", sha256="af50a126355a41c758fcda335a43fdb0a3cd97e608ba51c485afda3dd84a5b34")
 
     variant("metis", default=True, description="Enable/disable METIS support")
-    variant("caliper", default=True, description="Enable/disable Caliper support")
     variant("ofast", default=False, description="Enable gcc optimization flags")
 
     depends_on("mfem+mpi+metis", when="+metis")
     depends_on("mfem+mpi~metis", when="~metis")
-    depends_on("caliper", when="+caliper")
 
     depends_on("mfem@develop", when="@develop")
     depends_on("mfem@4.2.0:", when="@3.1")
@@ -52,7 +56,6 @@ class Laghos(MakefilePackage):
         targets = []
         spec = self.spec
 
-        targets.append("CALIPER_DIR=%s" % spec["caliper"].prefix)
         targets.append("MFEM_DIR=%s" % spec["mfem"].prefix)
         targets.append("CONFIG_MK=%s" % spec["mfem"].package.config_mk)
         targets.append("TEST_MK=%s" % spec["mfem"].package.test_mk)
