73c54bc5c6619e5b3a5f6fa4c27f54ed10f4d617
Olga T. Pearce <pearce8@llnl.gov>
2024-06-13 16:11:52 -0500

    Initial commit

---
+++ repo/scaleml-unet3d/package.py
@@ -0,0 +1,50 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from spack.package import *
+
+
+class ScaleML-UNet3D(CMakePackage, CudaPackage, ROCmPackage):
+    """ is a parallel ...
+    It requires an installation of PyTorch-[version] or higher.
+    """
+
+    tags = ["benchmark"]
+    homepage = "https://github.com/LLNL/AMG2023"
+    git = "https://github.com/LLNL/AMG2023.git"
+
+    license("Apache-2.0")
+
+    version("develop", branch="main")
+
+    variant("mpi", default=True, description="Enable MPI support")
+    variant("openmp", default=False, description="Enable OpenMP support")
+    variant("caliper", default=False, description="Enable Caliper monitoring")
+
+    depends_on("mpi", when="+mpi")
+    depends_on("hypre+mpi", when="+mpi")
+    requires("+mpi", when="^hypre+mpi")
+    depends_on("caliper", when="+caliper")
+    depends_on("adiak", when="+caliper")
+    depends_on("hypre+caliper", when="+caliper")
+    depends_on("hypre@2.31.0:")
+    depends_on("hypre+cuda", when="+cuda")
+    requires("+cuda", when="^hypre+cuda")
+    depends_on("hypre+rocm", when="+rocm")
+    requires("+rocm", when="^hypre+rocm")
+
+    def cmake_args(self):
+        cmake_options = []
+        cmake_options.append(self.define_from_variant("AMG_WITH_CALIPER", "caliper"))
+        cmake_options.append(self.define_from_variant("AMG_WITH_OMP", "openmp"))
+        cmake_options.append(self.define("HYPRE_PREFIX", self.spec["hypre"].prefix))
+        if self.spec["hypre"].satisfies("+cuda"):
+            cmake_options.append("-DAMG_WITH_CUDA=ON")
+        if self.spec["hypre"].satisfies("+rocm"):
+            cmake_options.append("-DAMG_WITH_HIP=ON")
+        if self.spec["hypre"].satisfies("+mpi"):
+            cmake_options.append("-DAMG_WITH_MPI=ON")
+
+        return cmake_options
