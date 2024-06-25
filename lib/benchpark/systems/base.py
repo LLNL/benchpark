314964016d9c9891633226d1d5a53802084eddc6
Peter Josef Scheibel <scheibel1@llnl.gov>
2024-06-24 17:31:53 -0700

    partial scaffolding for assembling external system config

--- lib/benchpark/systems/base.py
+++ lib/benchpark/systems/base.py
@@ -1,5 +1,15 @@
 # SPDX-License-Identifier: Apache-2.0
 
+import pathlib
+
+from benchpark.runtime import RuntimeResources
+
+
+class ScriptResources(RuntimeResources):
+    def __init__(self):
+        script_resource_root = pathlib.Path(os.path.abspath(__module__.__file__)).parents[3] / "external-resources"
+        super().__init__(script_resource_root)
+
 
 class SpackConfigMergeResolver:
     def __init__(self, benchpark_installation):
@@ -27,7 +37,7 @@ class SpackConfigMergeResolver:
 
 
 class System:
-    def __init__(self):
+    def __init__(self, input_dir):
         self.sys_cores_per_node = None
         self.sys_gpus_per_node = None
         self.sys_mem_per_node = None
@@ -37,7 +47,19 @@ class System:
 
         self.required = ["sys_cores_per_node", "scheduler", "timeout"]
 
-    def generate_system_description(self):
+    def generate_description(self, output_dir):
+        variables_yaml = output_dir / "variables.yaml"
+        with open(variables_yaml, "w") as f:
+            f.write(self.variables_yaml())
+
+    def externals(self, destdir):
+        # Each subdir of auxiliary_packages contains a set of mutually-exclusive
+        # configs. If the user specified nothing, we can combine the "first"
+        # file from each directory into a single packages config.
+        # (you can sort them alphabetically, choosing names like 01-rocm-543.yaml)
+        pass
+
+    def variables_yaml(self):
         for attr in self.required:
             if not getattr(self, attr, None):
                 raise ValueError(f"Missing required info: {attr}")
