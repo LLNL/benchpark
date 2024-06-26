64f3085523d9e1a1254c206dd8a283b524cc9f6b
Gregory Becker <becker33@llnl.gov>
2024-06-26 15:51:14 -0700

    first draft saxpy and experiment lifecycle methods

--- lib/benchpark/experiment.py
+++ lib/benchpark/experiment.py
@@ -6,6 +6,7 @@
 from typing import Dict, Tuple
 import os
 import inspect
+import yaml  # TODO: some way to ensure yaml available
 
 import benchpark.directives
 import benchpark.repo
@@ -214,3 +215,48 @@ class Experiment(metaclass=ExperimentMeta):
             return False
         s = self.extendee_spec
         return s and spec.satisfies(s)
+
+    def compute_include_section(self):
+        # include the config directory
+        # TODO: does this need to change to interop with System class
+        return ["./configs"]
+
+    def compute_config_section(self):
+        # default configs for all experiments
+        return {
+            "deprecated": True,
+            "spack_flags": {"install": "--add --keep-stage", "concretize": "-U -f"},
+        }
+
+    def compute_modifiers_section(self):
+        # by default we use the allocation modifier and no others
+        return [{"name": "allocation"}]
+
+    def compute_applications_section(self):
+        # TODO: is there some reasonable default?
+        raise NotImplementedError(
+            "Each experiment must implement compute_applications_section"
+        )
+
+    def compute_spack_section(self):
+        # TODO: is there some reasonable default based on known variable names?
+        raise NotImplementedError(
+            "Each experiment must implement compute_spack_section"
+        )
+
+    def compute_ramble_dict(self):
+        # This can be overridden by any subclass that needs more flexibility
+        return {
+            "ramble": {
+                "include": self.compute_include_section(),
+                "config": self.compute_config_section(),
+                "modifiers": self.compute_modifiers_section(),
+                "applications": self.compute_applications_section(),
+                "spack": self.compute_spack_section(),
+            }
+        }
+
+    def write_ramble_dict(self, filepath):
+        ramble_dict = self.compute_ramble_dict()
+        with open(filepath, "w") as f:
+            yaml.dump(ramble_dict, f)
