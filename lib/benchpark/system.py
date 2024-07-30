6418429d4990491affedab47ab4ae452fd8521dc
Peter Josef Scheibel <scheibel1@llnl.gov>
2024-07-30 14:30:16 -0700

    auto-gen compilers description

--- lib/benchpark/system.py
+++ lib/benchpark/system.py
@@ -26,13 +26,28 @@ import spack.util.spack_yaml as syaml  # noqa
 # individual because it is not used by Ramble
 # The following code block implements the line
 # import spack.schema.packages as packages_schema
-packages_schema_spec = importlib.util.spec_from_file_location(
+schemas = {
+    "spack.schema.packages": f"{bootstrapper.spack_location}/lib/spack/spack/schema/packages.py",
+    "spack.schema.compilers": f"{bootstrapper.spack_location}/lib/spack/spack/schema/compilers.py",
+}
+
+def load_schema(schema_id, schema_path):
+    schema_spec = importlib.util.spec_from_file_location(
+        schema_id, schema_path
+    )
+    schema = importlib.util.module_from_spec(schema_spec)
+    sys.modules[schema_id] = schema
+    schema_spec.loader.exec_module(schema)
+    return schema
+
+packages_schema = load_schema(
     "spack.schema.packages",
     f"{bootstrapper.spack_location}/lib/spack/spack/schema/packages.py",
 )
-packages_schema = importlib.util.module_from_spec(packages_schema_spec)
-sys.modules["spack.schema.packages"] = packages_schema
-packages_schema_spec.loader.exec_module(packages_schema)
+compilers_schema = load_schema(
+    "spack.schema.compilers",
+    f"{bootstrapper.spack_location}/lib/spack/spack/schema/compilers.py",
+)
 
 
 _repo_path = benchpark.repo.paths[benchpark.repo.ObjectTypes.systems]
@@ -72,36 +87,60 @@ class System:
         with open(variables_yaml, "w") as f:
             f.write(self.variables_yaml())
 
-        self.externals(output_dir)
+        self.external_packages(output_dir)
+        self.compilers(output_dir)
 
     def system_id(self):
         return _hash_id([self.variables_yaml()])
 
-    def externals(self, output_dir):
-        if not self.external_resources:
-            return
+    def _merge_config_files(self, schema, selections, dst_path):
+        data = cfg.read_config_file(selections[0], schema)
+        for selection in selections[1:]:
+            cfg.merge_yaml(
+                data, cfg.read_config_file(selection, schema)
+            )
 
-        # Each subdir of external-resources contains a mutually-exclusive
-        # set of package files
+        with open(dst_path, "w") as outstream:
+            syaml.dump_config(data, outstream)
+
+    def _select_components(self, basedir):
+        """Each subdir of `basedir` contains a mutually-exclusive set of
+        files: select one file from each component subdir.
+        """
         selections = list()
-        for component in os.listdir(self.external_resources):
-            component_dir = self.external_resources / component
+        for component in os.listdir(basedir):
+            component_dir = basedir / component
             component_choices = list(sorted(os.listdir(component_dir)))
             # TODO: for now, pick the first; need to allow users to select
             selections.append(component_dir / component_choices[0])
 
-        data = cfg.read_config_file(selections[0], packages_schema.schema)
-        for selection in selections[1:]:
-            cfg.merge_yaml(
-                data, cfg.read_config_file(selection, packages_schema.schema)
-            )
+        return selections
+
+    def external_packages(self, output_dir):
+        pkgs_basedir = pathlib.Path(self.resource_location) / "externals"
+        if not pkgs_basedir.exists():
+            return
+
+        selections = self._select_components(pkgs_basedir)
 
         aux = output_dir / "auxiliary_software_files"
-        os.mkdir(aux)
-
+        os.makedirs(aux, exist_ok=True)
         aux_packages = aux / "packages.yaml"
-        with open(aux_packages, "w") as outstream:
-            syaml.dump_config(data, outstream)
+
+        self._merge_config_files(packages_schema.schema, selections, aux_packages)
+
+    def compilers(self, output_dir):
+        compilers_basedir = pathlib.Path(self.resource_location) / "compilers"
+        if not compilers_basedir.exists():
+            return
+
+        selections = self._select_components(compilers_basedir)
+
+        aux = output_dir / "auxiliary_software_files"
+        os.makedirs(aux, exist_ok=True)
+        aux_compilers = aux / "compilers.yaml"
+
+        self._merge_config_files(compilers_schema.schema, selections, aux_compilers)
 
     def variables_yaml(self):
         for attr in self.required:
