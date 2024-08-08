76d9c47bce4393988613f88cbff62ddbae31fc9c
Riyaz Haque <your-email@example.com>
2024-08-07 22:10:59 -0700

    Enable caliper in mfem spack

--- repo/mfem/package.py
+++ repo/mfem/package.py
@@ -24,3 +24,15 @@ class Mfem(BuiltinMfem):
 
     version("4.1_comm_cali", branch="comm_cali", submodules=False, git="https://github.com/gracenansamba/mfem.git")
 
+    variant("caliper", default=False, description="Enable/disable Caliper support")
+    depends_on("caliper", when="+caliper")
+    depends_on("adiak", when="+caliper")
+
+    def get_make_config_options(self, spec, prefix):
+        def yes_no(varstr):
+            return "YES" if varstr in self.spec else "NO"
+        options = super(Mfem, self).get_make_config_options(spec, prefix)
+        options.append("MFEM_USE_CALIPER=%s" % yes_no("+caliper"))
+        if "+caliper" in self.spec: 
+            options.append("CALIPER_DIR=%s" % self.spec["caliper"].prefix)
+        return options
