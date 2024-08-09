1a28c1e20652d2a7f63c571631e62f072d45f927
august-knox <knox10@llnl.gov>
2024-08-09 10:49:44 -0700

    progress on strong and weak scaling for IOR

--- repo/ior/application.py
+++ repo/ior/application.py
@@ -16,15 +16,15 @@ class Ior(ExecutableApplication):
             'multi-node','single-node','mpi','network-latency-bound',
             'network-collectives','unstructured-grid']
 
-    executable('p', 'ior -Cge -wWr -F'+
+    executable('p', 'ior -Cge -vv -F -i5'+
             ' -b {b}' +
-            ' -t {t}' +
+            ' -t {t}' + 
             ' -a {a}'
             , use_mpi=True)
 
     workload('ior', executables=['p'])
 
-    workload_variable('a', default='POSIX',
+    workload_variable('a', default='MPIIO',
         description='api',
         workloads=['ior'])
 
@@ -39,14 +39,24 @@ class Ior(ExecutableApplication):
     workload_variable('N', default='1',
         description='numTasks -- number of tasks that are participating in the test (overrides MPI)',
         workloads=['ior'])
-    #TODO: build FOMs. ior measures "throughput", but not sure how to calculate that from results
-    figure_of_merit('Max write',
+    #TODO: Simplify FOMs
+    
+    figure_of_merit('Mean write OPs',
                     log_file='{experiment_run_dir}/{experiment_name}.out',
-                    fom_regex=r'Max Write:\s+(?P<fom>[0-9]+\.[0-9]*([0-9]*)?)',
+                    fom_regex=r'write\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+(?P<fom>[0-9]+\.[0-9]*([0-9]*)?)',
+                    group_name='fom', units='OPs')
+
+    figure_of_merit('Mean read OPs',
+                    log_file='{experiment_run_dir}/{experiment_name}.out',
+                    fom_regex=r'read\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+(?P<fom>[0-9]+\.[0-9]*([0-9]*)?)',
+                    group_name='fom', units='OPs')
+    figure_of_merit('Mean write',
+                    log_file='{experiment_run_dir}/{experiment_name}.out',
+                    fom_regex=r'write\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+(?P<fom>[0-9]+\.[0-9]*([0-9]*)?)',
                     group_name='fom', units='MiB/sec')
 
-    figure_of_merit('Max read',
+    figure_of_merit('Mean read',
                     log_file='{experiment_run_dir}/{experiment_name}.out',
-                    fom_regex=r'Max Read:\s+(?P<fom>[0-9]+\.[0-9]*([0-9]*)?)',
+                    fom_regex=r'read\s+[0-9]+\.[0-9]*[0-9]*\s+[0-9]+\.[0-9]*[0-9]*\s+(?P<fom>[0-9]+\.[0-9]*([0-9]*)?)',
                     group_name='fom', units='MiB/sec')
     success_criteria('pass', mode='string', match=r'.*', file='{experiment_run_dir}/{experiment_name}.out')
