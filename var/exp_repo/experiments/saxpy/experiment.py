eaf72d0d798869ee05a12389db1dc7c5e4523cfd
Gregory Becker <becker33@llnl.gov>
2024-08-22 12:32:33 -0700

    saxpy: fix mismatch between variable name and usage

--- var/exp_repo/experiments/saxpy/experiment.py
+++ var/exp_repo/experiments/saxpy/experiment.py
@@ -16,7 +16,7 @@ class Saxpy(Experiment):
 
         # GPU tests include some smaller sizes
         n = ["512", "1024"]
-        matrix = ["size"]
+        matrix = ["n"]
         if self.spec.satisfies("programming_model=openmp"):
             matrix += ["omp_num_threads"]
             variables["n_nodes"] = ["1", "2"]
