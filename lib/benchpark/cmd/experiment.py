f5d42ea528b23bbcdc9604318db47a5f518b94c5
Alec Scott <scott112@llnl.gov>
2024-08-21 17:14:53 -0700

    Add initial framework for experiment init

---
+++ lib/benchpark/cmd/experiment.py
@@ -0,0 +1,68 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+import os
+import shutil
+import sys
+
+import benchpark.experiment
+import benchpark.spec
+
+
+def experiment_init(args):
+    experiment_spec = benchpark.spec.ExperimentSpec(" ".join(args.spec))
+    experiment_spec = experiment_spec.concretize()
+
+    experiment = experiment_spec.experiment
+    experiment.initialize()
+
+    if args.basedir:
+        base = args.basedir
+        sysdir = experiment.experiment_id()
+        destdir = os.path.join(base, sysdir)
+    elif args.dest:
+        destdir = args.dest
+    else:
+        raise ValueError("Must specify one of: --dest, --basedir")
+
+    try:
+        os.mkdir(destdir)
+        experiment.generate_description(destdir)
+    except FileExistsError:
+        print(f"Abort: experiment description dir already exists ({destdir})")
+        sys.exit(1)
+    except Exception:
+        # If there was a failure, remove any partially-generated resources
+        shutil.rmtree(destdir)
+        raise
+
+
+def experiment_list(args):
+    raise NotImplementedError("'benchpark experiment list' is not available")
+
+
+def setup_parser(root_parser):
+    system_subparser = root_parser.add_subparsers(dest="experiment_subcommand")
+
+    init_parser = system_subparser.add_parser("init")
+    init_parser.add_argument("--dest", help="Place all system files here directly")
+    init_parser.add_argument(
+        "--basedir", help="Generate a system dir under this, and place all files there"
+    )
+
+    init_parser.add_argument("spec", nargs="+", help="Experiment spec")
+
+    system_subparser.add_parser("list")
+
+
+def command(args):
+    actions = {
+        "init": experiment_init,
+        "list": experiment_list,
+    }
+    if args.experiment_subcommand in actions:
+        actions[args.experiment_subcommand](args)
+    else:
+        raise ValueError(f"Unknown subcommand for 'system': {args.system_subcommand}")
