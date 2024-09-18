3f4e362859fe65e86013a267bf7074635da65ee9
august-knox <knox10@llnl.gov>
2024-09-18 15:34:52 -0700

    fixing license

--- repo/quicksilver/package.py
+++ repo/quicksilver/package.py
@@ -1,7 +1,8 @@
-# Copyright 2013-2024 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+# Copyright 2024 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+# SPDX-License-Identifier: Apache-2.0
+
 
 from spack.package import *
 
