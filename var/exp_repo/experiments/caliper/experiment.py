122fe005b6f12d74fa9da90296ebd3a7563e774c
Riyaz Haque <your-email@example.com>
2024-09-23 17:46:57 -0700

    Add license

--- var/exp_repo/experiments/caliper/experiment.py
+++ var/exp_repo/experiments/caliper/experiment.py
@@ -1,3 +1,9 @@
+# Copyright 2023 Lawrence Livermore National Security, LLC and other
+# Benchpark Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: Apache-2.0
+
+
 from benchpark.directives import variant
 from benchpark.experiment import Experiment
 
