b7eaae357cf68629cec3e978ebd512af6f734398
Gregory Becker <becker33@llnl.gov>
2024-09-30 14:58:43 -0700

    unit tests: update trivial failing test to xfail

--- lib/benchpark/test/trivial_test_test.py
+++ lib/benchpark/test/trivial_test_test.py
@@ -2,11 +2,13 @@
 # Benchpark Project Developers. See the top-level COPYRIGHT file for details.
 #
 # SPDX-License-Identifier: Apache-2.0
+import pytest
 
 
 def test_trivial_pass():
     assert True
 
 
+@pytest.mark.xfail
 def test_trivial_fail():
     assert False
