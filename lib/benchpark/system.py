517614b97aebcc1dd52d9c8ca8f40e60e273b617
Peter Josef Scheibel <scheibel1@llnl.gov>
2024-09-30 17:37:19 -0700

    remove now-unused variable

--- lib/benchpark/system.py
+++ lib/benchpark/system.py
@@ -157,7 +157,6 @@ system:
                 raise ValueError(f"Missing required info: {attr}")
 
         optionals = list()
-        optionals_as_cfg = ""
         for opt in ["sys_gpus_per_node", "sys_mem_per_node", "queue"]:
             if getattr(self, opt, None):
                 optionals.append(f"{opt}: {getattr(self, opt)}")
