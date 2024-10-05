b0fca4ea289e0c02e7c0ceba444be82b2441ea17
Riyaz Haque <your-email@example.com>
2024-10-05 09:00:55 -0700

    kripke scaling experiment

--- var/exp_repo/experiments/kripke/experiment.py
+++ var/exp_repo/experiments/kripke/experiment.py
@@ -19,8 +19,34 @@ class Kripke(ScalingExperiment, Experiment):
     )
 
     def compute_applications_section(self):
-        n_ranks = "{npx} * {npy} * {npz}"
-        n_threads_per_proc = 1
+        npx = "npx"
+        npy = "npy"
+        npz = "npz"
+        nzx = "nzx"
+        nzy = "nzy"
+        nzz = "nzz"
+        num_procs = f"{{{npx}}} * {{{npy}}} * {{{npz}}}"
+
+        variables = {}
+
+        if self.spec.satisfies("programming_model=openmp"):
+            variables["arch"] = "OpenMP"
+            variables["n_ranks"] = num_procs
+            variables["n_threads_per_proc"] = 1
+        elif self.spec.satisfies("programming_model=cuda"):
+            variables["arch"] = "CUDA"
+            variables["n_gpus"] = num_procs
+        elif self.spec.satisfies("programming_model=rocm"):
+            variables["arch"] = "HIP"
+            variables["n_gpus"] = num_procs
+
+        variables |= {
+            "ngroups": 64,
+            "gs": 1,
+            "nquad": 128,
+            "ds": 128,
+            "lorder": 4,
+        }
 
         # Number of processes in each dimension
         initial_np = [2, 2, 1]
@@ -29,52 +55,23 @@ class Kripke(ScalingExperiment, Experiment):
         initial_nz = [64, 64, 32]
 
         if self.spec.satisfies("scaling=single-node"):
-            npx = initial_np[0]
-            npy = initial_np[1]
-            npz = initial_np[2]
-
-            nzx = initial_nz[0]
-            nzy = initial_nz[1]
-            nzz = initial_nz[2]
+            variables[npx] = initial_np[0]
+            variables[npy] = initial_np[1]
+            variables[npz] = initial_np[2]
+            variables[nzx] = initial_nz[0]
+            variables[nzy] = initial_nz[1]
+            variables[nzz] = initial_nz[2]
         else:
             input_params = {}
-            if self.spec.satisfies("scaling=strong") or self.spec.satisfies("scaling=weak"):
-                input_params["np"] = initial_np
+            if self.spec.satisfies("scaling=strong"):
+                input_params[(npx, npy, npz)] = initial_np
+                variables[nzx] = initial_nz[0]
+                variables[nzy] = initial_nz[1]
+                variables[nzz] = initial_nz[2]
             if self.spec.satisfies("scaling=weak"):
-                input_params["nz"] = initial_nz
-            scaled_params = self.scale_experiment_variables(input_params, "np")
-            npx = scaled_params["np"][0]
-            npy = scaled_params["np"][1]
-            npz = scaled_params["np"][2]
-
-            nzx = scaled_params["nz"][0]
-            nzy = scaled_params["nz"][1]
-            nzz = scaled_params["nz"][2]
-
-        variables = {
-            "experiment_setup": "",
-            "n_ranks": n_ranks,
-            "n_threads_per_proc": n_threads_per_proc,
-            "ngroups": 64,
-            "gs": 1,
-            "nquad": 128,
-            "ds": 128,
-            "lorder": 4,
-            "nzx": nzx,
-            "nzy": nzy,
-            "nzz": nzz,
-            "npx": npx,
-            "npy": npy,
-            "npz": npz,
-        }
-
-        if self.spec.satisfies("programming_model=openmp"):
-            variables["arch"] = "OpenMP"
-        elif self.spec.satisfies("programming_model=cuda"):
-            variables["arch"] = "CUDA"
-            variables["n_gpus"] = n_ranks
-        elif self.spec.satisfies("programming_model=rocm"):
-            variables["arch"] = "HIP"
+                input_params[(npx, npy, npz)] = initial_np
+                input_params[(nzx, nzy, nzz)] = initial_nz
+            variables |= self.scale_experiment_variables(input_params, int(self.spec.variants['scaling-factor'][0]), int(self.spec.variants['scaling-iterations'][0]))
 
         experiment_name_template = f"kripke_{self.spec.variants['programming_model'][0]}_{self.spec.variants['scaling'][0]}"
         experiment_name_template += "_{n_nodes}_{n_ranks}_{n_threads_per_proc}_{ngroups}_{gs}_{nquad}_{ds}_{lorder}_{nzx}_{nzy}_{nzz}_{npx}_{npy}_{npz}"
@@ -97,28 +94,64 @@ class Kripke(ScalingExperiment, Experiment):
         }
 
     def compute_spack_section(self):
-        kripke_spec = "kripke@develop+mpi"
+        app_name = self.spec.name
+
+        # set package versions
+        app_version = "develop"
+        chai_version = "2024.02"
+        # caliper_version = "master"
+
+        # get system config options
+        # TODO: Get compiler/mpi/package handles directly from system.py
+        system_specs = {}
+        system_specs["compiler"] = "default-compiler"
+        system_specs["mpi"] = "default-mpi"
+        if self.spec.satisfies("programming_model=cuda"):
+            system_specs["cuda_version"] = "{default_cuda_version}"
+            system_specs["cuda_arch"] = "{cuda_arch}"
+        if self.spec.satisfies("programming_model=rocm"):
+            system_specs["rocm_arch"] = "{rocm_arch}"
+
+        # set package spack specs
+        package_specs = {}
+        if self.spec.satisfies("programming_model=cuda"):
+            package_specs["cuda"] = {
+                "pkg_spec": "cuda@{}+allow-unsupported-compilers".format(
+                    system_specs["cuda_version"]
+                ),
+                "compiler": system_specs["compiler"],
+            }
+        package_specs[system_specs["mpi"]] = (
+            {}
+        )  # empty package_specs value implies external package
+        package_specs["chai"] = {
+            "pkg_spec": f"chai@{chai_version} +mpi",
+            "compiler": system_specs["compiler"],
+        }
+        package_specs[app_name] = {
+            "pkg_spec": f"kripke@{app_version} +mpi",
+            "compiler": system_specs["compiler"],
+        }
+
         if self.spec.satisfies("programming_model=openmp"):
-            pass
+            package_specs["chai"]["pkg_spec"] += "+openmp"
+            package_specs[app_name]["pkg_spec"] += "+openmp"
         elif self.spec.satisfies("programming_model=cuda"):
-            pass
+            package_specs["chai"]["pkg_spec"] += "+cuda cuda_arch={}".format(
+                system_specs["cuda_arch"]
+            )
+            package_specs[app_name]["pkg_spec"] += "+cuda cuda_arch={}".format(
+                system_specs["cuda_arch"]
+            )
         elif self.spec.satisfies("programming_model=rocm"):
-            kripke_spec += "+rocm"
-        kripke_spec += "^chai@2024.02"
+            package_specs["chai"]["pkg_spec"] += "+rocm amdgpu_target={}".format(
+                system_specs["rocm_arch"]
+            )
+            package_specs[app_name]["pkg_spec"] += "+rocm amdgpu_target={}".format(
+                system_specs["rocm_arch"]
+            )
 
         return {
-            "packages": {
-                "kripke": {
-                    "pkg_spec": kripke_spec,
-                    "compiler": "default-compiler",
-                }
-            },
-            "environments": {
-                "kripke": {
-                    "packages": [
-                        "default-mpi",
-                        "kripke",
-                    ]
-                }
-            },
+            "packages": {k: v for k, v in package_specs.items() if v},
+            "environments": {app_name: {"packages": list(package_specs.keys())}},
         }
--- var/exp_repo/experiments/scalingexperiment/experiment.py
+++ var/exp_repo/experiments/scalingexperiment/experiment.py
@@ -16,80 +16,107 @@ class ScalingExperiment(object):
         description="Number of experiments to be generated",
     )
 
-    #def configure_scaling_policy(self, input_params: dict[str, int | list[int]], ordering_param:str):
-    def configure_scaling_policy(self, input_params, ordering_param):
+    # input parameters:
+    # 1. input_variables: dict[str, int | tuple(str), list[int]]. Input variables
+    # For the first value in input_variables, if the value is a list, select the
+    # index of its smallest element, 0 otherwise
+    # Beginning with this index, generate a list of indexes of length equal to
+    # the number of dimensions in an (ascending) round-robin order
+    #
+    # output:
+    # scaling_order: list[int]. list of num_exprs values, one for each dimension, 
+    # starting with the minimum value of the first element in input_variables arranged
+    # in an ascending round-robin order
+    def configure_scaling_policy(self, input_variables):
         # compute the number of dimensions
         n_dims = 1
-        for param in input_params.values():
+        for param in input_variables.values():
             if isinstance(param, list):
                 n_dims = len(param)
                 break
 
         # starting with the minimum value dim of the ordering parameter
         # compute the remaining n_dims-1 values in a round-robin manner
-        val = input_params[ordering_param]
+        val = input_variables[next(iter(input_variables))]
         min_dim = val.index(min(val)) if isinstance(val, list) else 0
 
         return [(min_dim + i) % n_dims for i in range(n_dims)]
 
-    # input_params: dict[str, int | list[int]]. Dictionary of all variables that need to be scaled in same order
-    # ordering_param: str. Name of the variable that decides the ordering. The default ordering is starting with the smallest dimension and proceeding in a RR manner
-    # scaling_factor: int. Factor by which to scale the variables. All variables in input_params are scaled by the same factor
-    # num_exprs: int. Number of experiments to be generated
-    # scaled_params: dict[str, list[list[int]]]. num_exprs values for each dimension of the input variable scaled 
-    # by the scaling_factor according to the scaling policy
-    #def scale_experiment_variables(self, input_params: dict[str, int | list[int]], ordering_param:str=None, scaling_factor=2, num_exprs=4):
-    def scale_experiment_variables(self, input_params, ordering_param=None, scaling_factor=2, num_exprs=4):
+    # input parameters:
+    # 1. input_variables: dict[str, int | tuple(str), list[int]]. Dictionary of all variables
+    # that need to be scaled. All variables are ordered as per the ordering policy of
+    # the first element in input_variables. By default, this policy is to scale the
+    # values beginning with the smallest dimension and proceeding in a RR manner through
+    # the other dimensions
+    #
+    # 2. scaling_factor: int. Factor by which to scale the variables. All entries in
+    # input_variables are scaled by the same factor
+    #
+    # 3. num_exprs: int. Number of experiments to be generated
+    #
+    # output:
+    # output_variables: dict[str, int | list[int]]. num_exprs values for each
+    # dimension of the input variable scaled by the scaling_factor according to the
+    # scaling policy
+    def scale_experiment_variables(self, input_variables, scaling_factor, num_exprs):
         # check if variable list is not empty
-        if not input_params:
+        if not input_variables:
             return {}
 
-        # if undefined, set ordering param to 
-        # the first param in the input_params dict
-        if not ordering_param:
-            ordering_param = next(iter(input_params))
-
-        # check if ordering_param is a valid key
-        if not ordering_param in input_params:
-            raise RuntimeError(
-                "Invalid ordering paramater"
-            )
-
         # check if:
-        # 1. input_params has the type dict[str, int | list[int]] and 
-        # 2. all the list variables have the same length i.e.
-        # all multi-valued variables have the same dimension
+        # 1. input_variables key value pairs are either of type str: int or tuple(str): list(int)
+        # 2. the length of key: tuple(str) is equal to length of value: list(int)
+        # 3. all values of type list(int) have the same length i.e. the same number of dimensions
         n_dims = None
-        for k, v in input_params.items():
-            if not isinstance(k, str):
-                raise RuntimeError(
-                    "Invalid key type. Must be a string."
-                )
-            if not (isinstance(v, int) or (isinstance(v, list) and all(isinstance(i, int) for i in v))):
-                raise RuntimeError(
-                    "Invalid value. Only int or list of int allowed."
-                )
-            if isinstance(v, list):
-                if not n_dims:
-                    n_dims = len(v)
-                if len(v) != n_dims:
+        for k, v in input_variables.items():
+            if isinstance(k, str):
+                if not isinstance(v, int):
                     raise RuntimeError(
-                        "Variables to be scaled have different dimensions"
+                        "Invalid key-value pair. Expected type str->int"
                     )
+            elif isinstance(k, tuple) and all(isinstance(s, str) for s in k):
+                if isinstance(v, list) and all(isinstance(i, int) for i in v):
+                    if len(k) != len(v):
+                        raise RuntimeError(
+                            "Invalid value. Length of key {k} does not match the length of value {v}"
+                        )
+                    else:
+                        if not n_dims:
+                            n_dims = len(v)
+                        if len(v) != n_dims:
+                            raise RuntimeError(
+                                "Variables to be scaled have different dimensions"
+                            )
+                else:
+                    raise RuntimeError(
+                        "Invalid key-value pair. Expected type tuple(str)->list[int]"
+                    )
+            else:
+                raise RuntimeError(
+                    "Invalid key. Expected type str or tuple(str)"
+                )
 
         # compute the scaling order based on the ordering_param
-        dim_scaling_order = self.configure_scaling_policy(input_params, ordering_param)
+        scaling_order_index = self.configure_scaling_policy(input_variables)
 
-        scaled_params = {}
-        for key, val in input_params.items():
-            scaled_params[key] = [[v] for v in val] if isinstance(val, list) else [[val]] 
+        scaled_variables = {}
+        for key, val in input_variables.items():
+            scaled_variables[key] = [[v] for v in val] if isinstance(val, list) else [[val]] 
 
         for exp_num in range(num_exprs - 1):
-            for param in scaled_params.values():
+            for param in scaled_variables.values():
                 if len(param) == 1:
                     param[0].append(param[0][-1]*scaling_factor)
                 else:
                     for p_idx, p_val in enumerate(param):
-                        p_val.append(p_val[-1]*scaling_factor if p_idx == dim_scaling_order[exp_num%len(dim_scaling_order)] else p_val[-1])
+                        p_val.append(p_val[-1]*scaling_factor if p_idx == scaling_order_index[exp_num%len(scaling_order_index)] else p_val[-1])
 
-        return scaled_params
+        output_variables = {}
+        for k, v in scaled_variables.items():
+            if isinstance(k, tuple):
+                for i in range(len(k)):
+                    output_variables[k[i]] = v[i] if len(v[i]) > 1 else v[i][0]
+            else:
+                output_variables[k] = v[0] if len(v[0]) > 1 else v[0][0]
+        return output_variables
+
