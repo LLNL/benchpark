9ffc15d1129439a2cdca6c04f65c0a3d0914b90f
Peter Josef Scheibel <scheibel1@llnl.gov>
2024-10-10 11:21:00 -0700

    basic check for method that must be implemented on subclass

--- lib/benchpark/cmd/audit.py
+++ lib/benchpark/cmd/audit.py
@@ -3,9 +3,24 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import benchpark.repo
+
+sys_repo = benchpark.repo.paths[benchpark.repo.ObjectTypes.systems]
+exp_repo = benchpark.repo.paths[benchpark.repo.ObjectTypes.experiments]
+
 def setup_parser(subparser):
     pass
 
 
+def audit_experiment(exp_cls):
+    required_methods = ["compute_applications_section"]
+
+    for method in required_methods:
+        if method  not in exp_cls.__dict__:
+            raise ValueError(f"{exp_class.__name__} does not implement {method}")
+
+
 def command(args):
-    pass
+    for exp_name in exp_repo.all_object_names():
+        exp_cls = exp_repo.get_obj_class(exp_name)
+        audit_experiment(exp_cls)
