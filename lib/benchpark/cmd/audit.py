5a749149b55368e2650cce35d64cf70a93ea15a9
Peter Josef Scheibel <scheibel1@llnl.gov>
2024-10-10 16:07:06 -0700

    verify that config files for each system are valid

--- lib/benchpark/cmd/audit.py
+++ lib/benchpark/cmd/audit.py
@@ -3,10 +3,14 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+import os
+import pathlib
 import sys
 
 import benchpark.repo
+import benchpark.system as system
 
+import ramble.config as cfg
 
 sys_repo = benchpark.repo.paths[benchpark.repo.ObjectTypes.systems]
 exp_repo = benchpark.repo.paths[benchpark.repo.ObjectTypes.experiments]
@@ -28,12 +32,39 @@ def audit_experiment(exp_cls):
     return errors
 
 
+def _find_yaml_files(d):
+    yaml_files = list()
+    for root, dirs, files in os.walk(d):
+        root = pathlib.Path(root)
+        yaml_files.extend((root / f) for f in files if f.endswith(".yaml"))
+    return yaml_files
+
+
+def audit_system(sys_cls):
+    errors = list()
+    basedir = pathlib.Path(sys_repo.filename_for_object_name(sys_cls.__name__)).parent
+    externals = basedir / "externals"
+    if externals.exists():
+        for f in _find_yaml_files(externals):
+            cfg.read_config_file(f, system.packages_schema)
+
+    compilers = basedir / "compilers"
+    if compilers.exists():
+        for f in _find_yaml_files(compilers):
+            cfg.read_config_file(f, system.compilers_schema)
+    return errors
+
+
 def command(args):
     all_errors = list()
 
     for exp_name in exp_repo.all_object_names():
         exp_cls = exp_repo.get_obj_class(exp_name)
         all_errors.extend(audit_experiment(exp_cls))
+
+    for sys_name in sys_repo.all_object_names():
+        sys_cls = sys_repo.get_obj_class(sys_name)
+        all_errors.extend(audit_system(sys_cls))
 
     for error in all_errors:
         print(error)
