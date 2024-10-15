f14d9a82f358b902e1c26a9af6503542a127203f
pearce8 <pearce8@llnl.gov>
2024-10-15 14:49:12 -0500

    Separating scaling and programming models in AMG experiment

--- var/exp_repo/experiments/amg2023/experiment.py
+++ var/exp_repo/experiments/amg2023/experiment.py
@@ -13,25 +13,16 @@ class Amg2023(Experiment):
     variant(
         "workload",
         default="problem1",
+        values=("problem1", "problem2"),
         description="problem1 or problem2",
     )
 
     variant(
         "experiment",
         default="throughput",
-        values=("strong", "weak", "throughput", "example"),
+        values=("strong", "weak", "throughput"),
         description="type of experiment",
     )
-
-    # def compute_modifiers_section(self):
-    #     modifier_list = super(Amg2023, self).compute_modifiers_section()
-    #     if not self.spec.satisfies("caliper=none"):
-    #         for var in list(self.spec.variants["caliper"]):
-    #             caliper_modifier_modes = {}
-    #             caliper_modifier_modes["name"] = "caliper"
-    #             caliper_modifier_modes["mode"] = var
-    #             modifier_list.append(caliper_modifier_modes)
-    #     return modifier_list
 
     def compute_applications_section(self):
         if self.spec.satisfies("workload=problem1"):
@@ -48,22 +39,16 @@ class Amg2023(Experiment):
         num_procs = f"{{{px}}} * {{{py}}} * {{{pz}}}"
 
         variables = {}
+        variables["n_ranks"] = num_procs
 
         # TODO: Use conditional variants
         # TODO: Implement supports for matrices, zips and excludes
         # TODO: Check for target system using programming_model mixin
         if self.spec.satisfies("programming_model=openmp"):
-            variables["n_ranks"] = num_procs
-            if self.spec.satisfies("experiment=example"):
-                variables["n_nodes"] = [2, 4]
-                variables["n_threads_per_proc"] = [4, 8, 16]
-            else:
-                variables["n_threads_per_proc"] = 1
+            variables["n_threads_per_proc"] = 1
             n_resources = "{n_ranks}_{n_threads_per_proc}"
-        elif self.spec.satisfies("programming_model=cuda"):
-            variables["n_gpus"] = num_procs
-            n_resources = "{n_gpus}"
-        elif self.spec.satisfies("programming_model=rocm"):
+        elif self.spec.satisfies("programming_model=cuda") or
+             self.spec.satisfies("programming_model=rocm"):
             variables["n_gpus"] = num_procs
             n_resources = "{n_gpus}"
 
@@ -72,79 +57,68 @@ class Amg2023(Experiment):
         experiment_setup = {}
         experiment_setup["variants"] = {"package_manager": "spack"}
 
-        # TODO: Use variant
         # Number of processes in each dimension
         initial_p = [2, 2, 2]
+        variables[px] = initial_p[0]
+        variables[py] = initial_p[1]
+        variables[pz] = initial_p[2]    
 
-        # TODO: Use variant
-        # Number of zones in each dimension, per process
-        if self.spec.satisfies("experiment=example"):
-            if self.spec.satisfies("programming_model=openmp"):
-                initial_n = [55, 110]
-            elif self.spec.satisfies("programming_model=cuda"):
-                initial_n = [10, 20]
-            elif self.spec.satisfies("programming_model=rocm"):
-                initial_n = [110, 220]
-            else:
-                initial_n = [10, 10, 10]
-        else:
-            initial_n = [10, 10, 10]
+        # Per-process size (in zones) in each dimension
+        initial_n = [80, 80, 80]
+        variables[nx] = initial_n[0]
+        variables[ny] = initial_n[1]
+        variables[nz] = initial_n[2]
+        
+        # TODO: Please explain the zips here.  Can we just declare this as a vector to begin with?
+        zips_size = "size"
+        experiment_setup["zips"] = {f"{zips_size}": [nx, ny, nz]}
 
-        if self.spec.satisfies("experiment=example"):
+        if self.spec.satisfies("programming_model=openmp"):
+            experiment_setup["matrices"] = [
+                {
+                    "size_nodes_threads": [
+                        f"{zips_size}",
+                        "n_nodes",
+                        "n_threads_per_proc",
+                    ]
+                   }
+            ]
+            experiment_setup["exclude"] = {
+                "where": [
+                    "{n_threads_per_proc} * {n_ranks} > {n_nodes} * {sys_cores_per_node}"
+                ]
+            }
+        elif self.spec.satisfies("programming_model=cuda") or 
+             self.spec.satisfies("programming_model=rocm"):
+            experiment_setup["matrix"] = [f"{zips_size}"]
+    
+        # TODO: Is there an order these code blocks need to be in?  
+        # Can the programming model part be completely separate from the scaling part?
+        input_params = {}
+        if self.spec.satisfies("experiment=throughput"):
             variables[px] = initial_p[0]
             variables[py] = initial_p[1]
             variables[pz] = initial_p[2]
-            variables[nx] = initial_n
-            variables[ny] = initial_n
-            variables[nz] = initial_n
-            zips_size = "size"
-            experiment_setup["zips"] = {f"{zips_size}": [nx, ny, nz]}
+            scaling_variable = (nx, ny, nz)
+            input_params[scaling_variable] = initial_n
+        elif self.spec.satisfies("experiment=strong"):
+            scaling_variable = (px, py, pz)
+            input_params[scaling_variable] = initial_p
+            variables[nx] = initial_n[0]
+            variables[ny] = initial_n[1]
+            variables[nz] = initial_n[2]
+        elif self.spec.satisfies("experiment=weak"):
+            scaling_variable = (px, py, pz)
+            input_params[scaling_variable] = initial_p
+            input_params[(nx, ny, nz)] = initial_n
+        variables |= self.scale_experiment_variables(
+            input_params,
+            int(self.spec.variants["scaling-factor"][0]),
+            int(self.spec.variants["scaling-iterations"][0]),
+            scaling_variable,
+        )
 
-            if self.spec.satisfies("programming_model=openmp"):
-                experiment_setup["matrices"] = [
-                    {
-                        "size_nodes_threads": [
-                            f"{zips_size}",
-                            "n_nodes",
-                            "n_threads_per_proc",
-                        ]
-                    }
-                ]
-            elif self.spec.satisfies("programming_model=cuda") or self.spec.satisfies(
-                "programming_model=rocm"
-            ):
-                experiment_setup["matrix"] = [f"{zips_size}"]
-            if self.spec.satisfies("programming_model=openmp"):
-                experiment_setup["exclude"] = {
-                    "where": [
-                        "{n_threads_per_proc} * {n_ranks} > {n_nodes} * {sys_cores_per_node}"
-                    ]
-                }
-        else:
-            input_params = {}
-            if self.spec.satisfies("experiment=throughput"):
-                variables[px] = initial_p[0]
-                variables[py] = initial_p[1]
-                variables[pz] = initial_p[2]
-                scaling_variable = (nx, ny, nz)
-                input_params[scaling_variable] = initial_n
-            elif self.spec.satisfies("experiment=strong"):
-                scaling_variable = (px, py, pz)
-                input_params[scaling_variable] = initial_p
-                variables[nx] = initial_n[0]
-                variables[ny] = initial_n[1]
-                variables[nz] = initial_n[2]
-            elif self.spec.satisfies("experiment=weak"):
-                scaling_variable = (px, py, pz)
-                input_params[scaling_variable] = initial_p
-                input_params[(nx, ny, nz)] = initial_n
-            variables |= self.scale_experiment_variables(
-                input_params,
-                int(self.spec.variants["scaling-factor"][0]),
-                int(self.spec.variants["scaling-iterations"][0]),
-                scaling_variable,
-            )
-
+        # TODO: Add explanation
         experiment_setup["variables"] = variables
 
         return {
